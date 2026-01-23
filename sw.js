3b14c8e1eac1639585e641c78628e9c7ec841e0e
danipad100 <danipad100@gmail.com>
2026-01-23 01:39:11 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
-const CACHE='divisas-bcv-v3';
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html'])));
+
+const CACHE_NAME = 'divisas-bcv-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-180.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
 });
